4aaa56d1673b201163c5a87bdb333cba2d8140bf
rdelonzek <renidelonzek@gmail.com>
2021-04-09 16:08:23 -0300

    fix: path windows

--- sqflite_common_ffi/lib/src/windows/setup.dart
+++ sqflite_common_ffi/lib/src/windows/setup.dart
@@ -1,11 +1,10 @@
 import 'dart:ffi';
 import 'dart:io';
 
+import 'package:path/path.dart';
 import 'package:sqflite_common_ffi/src/import.dart';
 import 'package:sqlite3/open.dart';
 import 'package:sqlite3/sqlite3.dart';
-import 'package:path/path.dart';
-import 'package:sqflite_common_ffi/src/windows/setup_impl.dart';
 
 /// Windows specific sqflite3 initialization.
 ///
@@ -16,20 +15,27 @@ import 'package:sqflite_common_ffi/src/windows/setup_impl.dart';
 ///
 /// This code is only provided for reference. See package [`sqlite3`](https://pub.dev/packages/sqlite3)
 /// for more information.
+
 void windowsInit() {
+  String rootPath;
   if (isDebug) {
-    var location = findPackagePath(Directory.current.path)!;
-    var path = normalize(join(location, 'src', 'windows', 'sqlite3.dll'));
-    open.overrideFor(OperatingSystem.windows, () {
-      // devPrint('loading $path');
-      try {
-        return DynamicLibrary.open(path);
-      } catch (e) {
-        stderr.writeln('Failed to load sqlite3.dll at $path');
-        rethrow;
-      }
-    });
+    rootPath = 'build';
+  } else {
+    rootPath = 'data';
   }
+  var location = Directory.current.path;
+  var path = normalize(join(location, rootPath, 'flutter_assets', 'packages',
+      'sqflite_common_ffi', 'assets', 'sqlite3.dll'));
+
+  open.overrideFor(OperatingSystem.windows, () {
+    // devPrint('loading $path');
+    try {
+      return DynamicLibrary.open(path);
+    } catch (e) {
+      stderr.writeln('Failed to load sqlite3.dll at $path');
+      rethrow;
+    }
+  });
 
   // Force an open in the main isolate
   // Loading from an isolate seems to break on windows
